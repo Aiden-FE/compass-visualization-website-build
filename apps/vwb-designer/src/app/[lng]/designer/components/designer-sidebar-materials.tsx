5ff92a5984957cb636f2a8179d58fb30a82ee116
Aiden <aiden_jin@outlook.com>
2023-08-11 21:12:36 +0800

    test: update docker container

--- apps/vwb-designer/src/app/[lng]/designer/components/designer-sidebar-materials.tsx
+++ apps/vwb-designer/src/app/[lng]/designer/components/designer-sidebar-materials.tsx
@@ -16,26 +16,29 @@ function DesignerSidebarMaterials() {
   }
 
   return (
-    <Card title="Card Title">
-      <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 1 })} style={gridStyle}>
-        Content
-      </Card.Grid>
-      <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 1 })} style={gridStyle}>
-        Content
-      </Card.Grid>
-      <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 1 })} style={gridStyle}>
-        Content
-      </Card.Grid>
-      <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 2 })} style={gridStyle}>
-        Content
-      </Card.Grid>
-      <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 2 })} style={gridStyle}>
-        Content
-      </Card.Grid>
-      <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 2 })} style={gridStyle}>
-        Content
-      </Card.Grid>
-    </Card>
+    <>
+      <Card title="基础组件">
+        <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 1 })} style={gridStyle}>
+          组件1
+        </Card.Grid>
+        <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 1 })} style={gridStyle}>
+          组件1
+        </Card.Grid>
+        <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 1 })} style={gridStyle}>
+          组件1
+        </Card.Grid>
+        <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 2 })} style={gridStyle}>
+          组件2
+        </Card.Grid>
+        <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 2 })} style={gridStyle}>
+          组件2
+        </Card.Grid>
+        <Card.Grid draggable unselectable="on" onDragStart={(e) => onDragStart(e, { w: 2, h: 2 })} style={gridStyle}>
+          组件2
+        </Card.Grid>
+      </Card>
+      <p className="text-center text-gray-500 text-[12px]">拖动组件到中间布局区域</p>
+    </>
   );
 }
 
