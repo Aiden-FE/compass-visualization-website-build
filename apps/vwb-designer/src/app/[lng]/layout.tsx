1fb6f2c61581c5d7130365851926ae9696f3b3b7
Aiden <aiden_jin@outlook.com>
2023-08-11 22:23:29 +0800

    feat: 更换system cdn地址

--- apps/vwb-designer/src/app/[lng]/layout.tsx
+++ apps/vwb-designer/src/app/[lng]/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang={lng} dir={dir(lng)}>
       <head>
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
-        <script src="https://cdn.jsdelivr.net/npm/systemjs@6.14.1/dist/system.min.js" />
+        <script src="https://aidenoss.cpolar.cn/compass-open/3rd/systemjs/system.min.js" />
       </head>
       <body className={`${inter.className} cp-scrollbar cp-scrollbar_fixed`} suppressHydrationWarning>
         <AppWrap>{children}</AppWrap>
